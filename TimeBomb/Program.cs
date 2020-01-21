4d6e2add183f70dc8cdebc41bbef504d7dc3e81f
JiKon <canyounotspammethanks@gmail.com>
2020-01-21 13:37:48 +0100

    PRO:B2:L6

---
+++ TimeBomb/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TimeBomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear(); 
+
+            for (int i = 5; i >= 1; i--)
+            {
+                Console.WriteLine("Bomb goes off in {0}", i);
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("BOOOOOOOM!");
+            
+        }
+    }
+}
+
