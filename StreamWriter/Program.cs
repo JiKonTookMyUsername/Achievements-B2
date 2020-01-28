50ccadae08cbd084fc83d58e87e625e1d8581da0
JiKon <canyounotspammethanks@gmail.com>
2020-01-28 10:56:59 +0100

    PRO:B2:L1

---
+++ StreamWriter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace streamwriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[3]; // Array to hold the names 
+
+            Console.WriteLine("Typ here your top 3 TV shows: ");
+
+            for (int i = 0; i < 3; i++)
+            {
+                names[i] = Console.ReadLine();
+                WriteToFile(names[i]);
+            }
+            
+
+        }
+        static void WriteToFile(string txt) {
+            // Define our one and only variable
+           
+
+            // Write to text file
+
+            StreamWriter writeToFile = new StreamWriter(@"top3.txt");
+
+            
+            writeToFile.WriteLine(txt);
+            
+
+            writeToFile.Close();
+
+
+           
+
+        }
+    }
+}
